0dca05590d3f98f883b98a59665c05dc73085640
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-20 15:19:53 +0800

    Update testaddfund.vue

--- pages/testaddfund.vue
+++ pages/testaddfund.vue
@@ -6,16 +6,18 @@
 
 <script>
     const Moralis = require("moralis")
-    import { mapState } from 'vuex'
+    import { mapState,mapMutations } from 'vuex'
     export default {
         computed:{
             ...mapState(['pointbalance'])
         },
         methods:{
+            ...mapMutations(['set_balance']),
             async addFund(){
                 const user = await Moralis.User.current();
                 let addFunds = this.pointbalance + 1;
-                user.set("balance",3)
+                this.set_balance(addFunds)
+                user.set("balance",addFunds)
                 user.save()
             }
         },
