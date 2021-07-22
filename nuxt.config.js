0d509f17749afc4877c433bd243c371ff6e69a1e
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-22 21:19:35 +0800

    Updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - axie',
-    title: 'axie',
+    titleTemplate: '%s - Marketplace',
+    title: 'Axie Overload',
     htmlAttrs: {
       lang: 'en'
     },
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
